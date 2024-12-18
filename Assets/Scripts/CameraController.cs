4a7862f878a990e430fda4407a3959f1aa332859
HI-GRU <gruns0989@naver.com>
2024-12-18 23:36:34 +0900

    [asset] 에셋 삭제

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,18 +2,26 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
+    [SerializeField]
+    private GameObject player;
+    public float lerpSpeed = 1.0F;
+
+    private Vector3 offset;
+
+    private Vector3 targetPos;
+
+    private void Start()
+    {
+        if (player == null) return;
+
+        offset = transform.position - player.transform.position;
+    }
 
     private void Update()
     {
         if (player == null) return;
 
-        Vector3 pos = new Vector3(
-            player.transform.position.x,
-            player.transform.position.y,
-            transform.position.z
-        );
-
-        transform.position = pos;
+        targetPos = player.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
     }
 }
