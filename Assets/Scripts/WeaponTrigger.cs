aa9f959b801076a8ef64f643a15daec0ad712098
parseley <gruns0989@naver.com>
2024-12-21 22:42:14 +0900

    [feat] 충돌 시 폭발 이펙트 구현

--- Assets/Scripts/WeaponTrigger.cs
+++ Assets/Scripts/WeaponTrigger.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 
 public class WeaponTrigger : MonoBehaviour
 {
+    [Header("Explosion Option")]
+    [SerializeField] private GameObject explosionParticlePrefab;
+    [SerializeField] private float playerExplosionSize;
+    [SerializeField] private float weaponExplosionSize;
+
+    Vector2 collisionPoint;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
+        collisionPoint = other.ClosestPoint(transform.position);
+
         if (other.CompareTag("Player Skin"))
         {
             HandlePlayerCollision(other);
@@ -20,13 +29,24 @@ public class WeaponTrigger : MonoBehaviour
 
     private void HandlePlayerCollision(Collider2D playerSkin)
     {
+        Explosion(collisionPoint, playerExplosionSize);
         Destroy(playerSkin.GetComponentInParent<Player>().gameObject);
         Destroy(gameObject);
     }
 
     private void HandleWeaponCollision(Collider2D otherWeapon)
     {
+        Explosion(collisionPoint, weaponExplosionSize);
         Destroy(otherWeapon.gameObject);
         Destroy(gameObject);
+    }
+
+    private void Explosion(Vector3 position, float scale)
+    {
+        if (explosionParticlePrefab != null)
+        {
+            GameObject effect = Instantiate(explosionParticlePrefab, position, Quaternion.identity);
+            effect.transform.localScale = Vector3.one * scale;
+        }
     }
 }
