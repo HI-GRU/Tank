a695a0118bc94a18b27a43395c55d8dcd372c3ef
parseley <gruns0989@naver.com>
2024-12-22 01:39:13 +0900

    [feat] Obstacle 생성 로직 구현 예정

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [Header("Obstacles Option")]
+    [SerializeField] private int numOfTypes;
+    [SerializeField] private List<GameObject> pyramids;
+    private int type;
+
+    // private void SetType()
+    // {
+    //     type = Random.Range(1, numOfTypes);
+
+    //     switch (type)
+    //     {
+    //         case 1:
+    //             currentType = pyramids;
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
+}
